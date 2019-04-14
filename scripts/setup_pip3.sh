6b285b4ef6f65f194a6d30788ca09aedc25dbf0a
progrunner17 <g.sho1500@gmail.com>
2019-04-15 00:54:33 +0900

    change python&pip setups

---
+++ scripts/setup_pip3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -eu
+sudo apt install python3-pip -y
+sudo pip3 install --upgrade pip
--- scripts/setup_python3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#install python bindings (psycopg2 package)
-sudo apt install python3-pip -y
-sudo pip3 install -U pip
-sudo pip3 install psycopg2
