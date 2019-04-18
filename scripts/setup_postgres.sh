bfb7cf8fb3a0530a70e9ba83c0eba5aa1568c8ef
progrunner17 <g.sho1500@gmail.com>
2019-04-18 18:34:02 +0900

    update setup postgres

--- scripts/setup_postgres.sh
+++ scripts/setup_postgres.sh
@@ -9,10 +9,12 @@ sudo sed -i 's/^local\s*all\s*all\s*peer/local\tall\tall\tmd5/g'\
   /etc/postgresql/9.5/main/pg_hba.conf
 sudo systemctl restart postgresql
 
-sudo -u postgres psql -c "CREATE ROLE honomara WITH LOGIN PASSWORD 'honomara';"
-sudo -u postgres psql -c "CREATE DATABASE honomara ENCODING=UTF8 OWNER=honomara;"
 
-if ! (type pip3 > /dev/null 2>&1); then
-./setup_pip3.sh
+if ! (python3 -c 'import psycopg2'); then
+  if ! (type pip3 > /dev/null 2>&1); then
+  ./setup_pip3.sh
+  fi
+  sudo pip3 install psycopg2
+  sudo -u postgres psql -c "CREATE ROLE honomara WITH LOGIN PASSWORD 'honomara';"
+  sudo -u postgres psql -c "CREATE DATABASE honomara ENCODING=UTF8 OWNER=honomara;"
 fi
-sudo pip3 install psycopg2
