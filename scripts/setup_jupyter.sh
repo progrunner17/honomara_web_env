38dab2914000e7c01fa3751324c32645beee1776
progrunner17 <g.sho1500@gmail.com>
2019-04-18 17:36:11 +0900

    add bash kernel for jupyter

--- scripts/setup_jupyter.sh
+++ scripts/setup_jupyter.sh
@@ -5,6 +5,8 @@ if ! (type pip3 > /dev/null 2>&1); then
 ./setup_pip3.sh
 fi
 sudo pip3 install jupyter 
+sudo pip3 install bash_kernel 
+sudo python3 -m bash_kernel.install
 
 HOME=/home/vagrant
 mkdir -p $HOME/.jupyter
