92fa0e782f1feca021207d0650854e47734b65df
progrunner17 <g.sho1500@gmail.com>
2019-04-18 17:43:26 +0900

    split apt install in setup web server

--- scripts/setup_web_server.sh
+++ scripts/setup_web_server.sh
@@ -2,6 +2,8 @@
 set -eu
 sudo apt-add-repository -y ppa:ondrej/php
 sudo apt update -y
-sudo apt install -y  apache2 libapache2-mod-php7.3 php7.3 php7.3-cli \
-php7.3-common php7.3-pgsql
+sudo apt install -y  apache2
+sudo apt install -y libapache2-mod-php7.3 php7.3 php7.3-cli php7.3-common
+sudo apt install -y php7.3-pgsql
+sudo apt install -y php7.3-mysql
 sudo chmod a+rw /var/www/html
