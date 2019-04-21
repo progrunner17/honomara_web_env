70d44658ad3199a42fe576422b94e41d8b39268c
progrunner17 <g.sho1500@gmail.com>
2019-04-21 14:18:00 +0900

    small fix

--- scripts/setup_mysql.sh
+++ scripts/setup_mysql.sh
@@ -4,7 +4,14 @@ set -eu
 echo "mysql-server-5.7 mysql-server/root_password password honomara" |sudo debconf-set-selections -
 echo "mysql-server-5.7 mysql-server/root_password_again password honomara" |sudo debconf-set-selections -
 sudo apt install  mysql-server-5.7 mysql-client-5.7 -y
-if ! (type pip3 > /dev/null 2>&1); then
-./setup_pip3.sh
+
+
+if ! (pythin3 -c 'import mysql.connector'); then
+  if ! (type pip3 > /dev/null 2>&1); then
+  ./setup_pip3.sh
+  fi
+  sudo pip3 install mysql-connector-python
+  mysql  -u root --password='honomara' -e "CREATE DATABASE IF NOT EXISTS honomara CHARACTER SET 'utf8';"
+  mysql  -u root --password='honomara' -e "CREATE USER IF NOT EXISTS honomara@localhost IDENTIFIED BY honomara;"
+  mysql  -u root --password='honomara' -e "GRANT ALL PRIVILEGES ON honomara.* TO  honomara@localhost;"
 fi
-sudo pip3 install mysql-connector-python
